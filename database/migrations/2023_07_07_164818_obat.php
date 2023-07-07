ea04c210e4630aa69df1e5c3dc070cb388b3e6fc
fajarmagsyar <fajar.magsyar@gmail.com>
2023-07-07 23:55:45 +0700

    migration untuk obat

---
+++ database/migrations/2023_07_07_164818_obat.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class Obat extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
+        Schema::create('obat', function (Blueprint $table) {
+            $table->uuid('obat_id')->primary();
+            $table->string('kode_obat')->nullable();
+            $table->string('nama_obat')->nullable();
+            $table->string('kemasan')->nullable();
+            $table->integer('stok_awal')->integer();
+            $table->integer('stok_saat_ini')->integer();
+            $table->integer('hdj')->integer();
+            $table->text('keterangan')->integer();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        DB::statement('ALTER TABLE obat ALTER COLUMN obat_id SET DEFAULT uuid_generate_v4();');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('obat');
+    }
+}
